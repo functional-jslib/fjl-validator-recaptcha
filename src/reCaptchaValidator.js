60c8fb800606cc7e7c8789ec7047fb825a6a3330
Ely De La Cruz <elycruz@elycruz.com>
2018-01-21 01:09:33 -0500

    Added progress on tests (added mock-server, puppeteer usage and a mind-dump of approach to testing JSON returned from recaptcha validation on server.

--- src/reCaptchaValidator.js
+++ src/reCaptchaValidator.js
@@ -1,10 +1,24 @@
 /**
  * Created by elydelacruz on 6/10/16.
+ * @recaptchaVersion v2
+ * @reference see below:
+ * @see https://developers.google.com/recaptcha/docs/
+ * @see https://developers.google.com/recaptcha/docs/verify
+ * @notes:
+ * The response is a JSON object, for reCAPTCHA V2 and invisible reCAPTCHA:
+ * ```
+ * {
+ *   "success": true|false,
+ *   "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+ *   "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+ *   "error-codes": [...]        // optional
+ * }
+ * ```
  */
 import {https} from 'https';
 import {queryString} from 'querystring';
 import {defineEnumProps$} from 'fjl-mutable';
-import {getErrorMsgByKey, toValidationOptions, toValidationResult} from 'fjl-validator';
+import {getErrorMsgByKey, toValidationResult, defaultValueObscurator} from 'fjl-validator';
 import {assignDeep, isEmpty} from 'fjl';
 
 export const
@@ -14,34 +28,37 @@ export const
     INVALID_INPUT_SECRET = 'invalid-input-secret',
     MISSING_INPUT_RESPONSE = 'missing-input-response',
     INVALID_INPUT_RESPONSE = 'invalid-input-response',
+    BAD_REQUEST = 'bad-request',
     UNKNOWN_ERROR = 'unknown-error',
 
     toReCaptchaOptions = options =>
-        toValidationOptions(
-            defineEnumProps$([
-                    [String, 'secret'],
-                    [String, 'remoteip'],
-                    [String, 'response'],
-                    [Boolean, 'async', true],
-                    [Object, 'requestOptions', {
-                        host: 'www.google.com',
-                        path: '/recaptcha/api/siteverify',
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        }
-                    }]
-                ], {
+        defineEnumProps$([
+                [String, 'secret'],
+                [String, 'remoteip'],
+                [String, 'response'],
+                [Boolean, 'async', true],
+                [Object, 'requestOptions', {
+                    host: 'www.google.com',
+                    path: '/recaptcha/api/siteverify',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    }
+                }],
+                [Object, 'messageTemplates', {}],
+                [Boolean, 'valueObscured', false],
+                [Function, 'valueObscurator', defaultValueObscurator]
+            ], assignDeep({
                 messageTemplates: {
                     [INVALID_SUBMISSION]: 'The submitted recaptcha submission is invalid/did-not-pass-validation.',
                     [MISSING_INPUT_SECRET]: 'The secret parameter is missing.',
                     [INVALID_INPUT_SECRET]: 'The secret parameter is invalid or malformed.',
                     [MISSING_INPUT_RESPONSE]: 'The response parameter is missing.',
                     [INVALID_INPUT_RESPONSE]: 'The response parameter is invalid or malformed.',
+                    [BAD_REQUEST]: 'Bad request',
                     [UNKNOWN_ERROR]: 'Unknown error.'
                 }
-            }),
-            options || {}
+            }, options || {})
         ),
 
     reCaptchaIOValidator = (options, value) => {
--- tests/test-reCaptchaValidator.js
+++ tests/test-reCaptchaValidator.js
@@ -12,29 +12,63 @@ import {
 } from '../src/reCaptchaValidator';
 
 import {recaptchaKeys} from '../package.json';
-
 import {log, jsonClone, runHasPropTypes} from './utils';
+import puppeteer from 'puppeteer';
+// import {requestP} from 'request-promise-native';
 
 log('Received recaptcha keys: ', recaptchaKeys);
 
+let browser;
+
+jest.setTimeout(21000);
+
 describe ('reCaptchaValidator', function () {
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+    });
+
     describe ('#toReCaptchaOptions', function () {
-        describe ('properties', function () {
-            [{}, undefined].forEach(value => {
-                runHasPropTypes([
-                    [String, 'secret', ['hello', 99]],
-                    [String, 'remoteip', ['hello', false]],
-                    [String, 'response', ['hello', false]],
-                    [Boolean, 'async', [false, '']],
-                    [Object, 'requestOptions', [{}, false]],
-                    [Object, 'messageTemplates', [{}, false]]
-                ], toReCaptchaOptions(value));
-            });
+        [{}, undefined].forEach(value => {
+            runHasPropTypes([
+                [String, 'secret', ['hello', 99]],
+                [String, 'remoteip', ['hello', false]],
+                [String, 'response', ['hello', false]],
+                [Boolean, 'async', [false, '']],
+                [Object, 'requestOptions', [{}, false]],
+                [Object, 'messageTemplates', [{}, false]]
+            ], toReCaptchaOptions(value));
         });
     });
 
     describe ('#makeReCaptchaRequest', function () {
-        test ('should have more tests');
+        test ('should have more tests', async function () {
+            const page = await browser.newPage();
+            await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/60.0.3112.113 Chrome/60.0.3112.113 Safari/537.36')
+            await page.goto('http://localhost:3000/test-recaptcha-validator.html');
+            await page.waitFor(3000);
+            const anchorName = '.rc-anchor-content',
+                recaptchaUiFrame = page.mainFrame().childFrames().shift();
+            await recaptchaUiFrame.waitFor(anchorName);
+            const $anchor = await recaptchaUiFrame.$(anchorName);
+            await $anchor.click();
+            await page.waitFor(3000);
+            await page.screenshot({path: 'example.png'});
+            await page.click('input[type="submit"]');
+            await page.waitFor(3000);
+            await page.screenshot({path: 'example2.png'});
+            // dumpFrameTree(page.mainFrame(), '');
+            //
+            // function dumpFrameTree(frame, indent) {
+            //     console.log(indent + frame.url());
+            //     for (let child of frame.childFrames())
+            //         dumpFrameTree(child, indent + '  ');
+            // }
+            await browser.close();
+            // makeReCaptchaRequest(toReCaptchaOptions({
+            //     secret: recaptchaKeys.secret
+            // }))
+        });
     });
 
     describe ('#reCaptchaIOValidator', function () {
